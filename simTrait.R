5e14d973cc94aabc2090e7c95137423534f29f32
oliveirab <brunno.oliveira@me.com>
2016-02-10 16:05:22 -0300

    .

--- simTrait.R
+++ simTrait.R
@@ -1,26 +1,38 @@
 # Source code for simulation assemblages and calculating FD metrics.
-# Originaly submited in:
-# Oliveira et al., 2016 Species and functional diversity accumulate differently in
-# terrestrial mammals. Global Ecology and Biogeography
+# Supplementary material in: Species and functional diversity accumulate differently in terrestrial mammals'. Global Ecology and Biogeography, 2016 - Submitted
+# Authors: Brunno F. Oliveira, Antonin Machac, Gabriel C. Costa, Thomas M. Brooks, Ana D. Davidson, Carlo Rondinini, Catherine H. Graham.
+# contact brunno.oliveira@me.com for any further question.
 
-################################################################
+library(FD)
 
-require(FD)
+setwd("~/Dropbox/Doutorado Brunno/Manuscritos/Chap1 Age and FD/TraitSim/GitHub_repo_simulation_traits")
 
-rm(list=ls())
+load("simTrait.RData")
 
-#### Set WD
-setwd('/home/brunno/Dropbox/Doutorado Brunno/Manuscritos/Chap1 Age and FD/TraitSim')
+###########################################
+# Load functions:
+
+source("simul_dbFB_mod.R") # Modified version of simul_dbFD function from FD packages
+# Available at: https://github.com/oliveirab/simulation_traits/blob/master/simul_dbFB_mod.R
+# The modified version of the simul.dbFD function from FD package allows the choice of
+# parameters to generate trait values, which is not possible with the orinal simul.dbFD
+# function.  
+# To simulate traits using simul.dbFD.mod insert parameters in the argument tr.par, separated
+# by comma, as follow:  
+# If tr.method = 'norm' .:. tr.par = c(mean, sd)  
+# If tr.method = 'lnorm' .:. tr.par = c(mean, sd)  
+# If tr.method = 'unif' .:. tr.par = c(min, max)   
+# If tr.method = 'exp' .:. tr.par = rate  
 
 # Pairs correlation
-# P-value and R coefficient
+# P-value and r coefficient
 panel.cor <- function(x, y, digits=2, cex.cor)
 {
   usr <- par("usr"); on.exit(par(usr))
   par(usr = c(0, 1, 0, 1))
-  r <- abs(cor(x, y))
+  r <- abs(cor(x, y,method = "spearman"))
   txt <- format(c(r, 0.123456789), digits=digits)[1]
-  test <- cor.test(x,y)
+  test <- cor.test(x,y,method = "spearman")
   Signif <- ifelse(round(test$p.value,3)<0.001,"p<0.001",paste("p=",round(test$p.value,3)))  
   text(0.5, 0.25, paste("r=",txt))
   text(.5, .75, Signif)
@@ -45,38 +57,122 @@ panel.hist <- function(x, ...)
   y <- h$counts; y <- y/max(y)
   rect(breaks[-nB], 0, breaks[-1], y, col="white", ...)
 }
+```
 
-### CALCULATE FD METRICS
+###########################################
+# Simulate assemblages and calculate FD metrics:
+###########################################
 
-# s - vector listing the different levels of species richness used in the simulations
-# t	- number of traits
-# r	- number of replicates per species richness level
-# p	- number of species in the common species pool
-# tr.method	- character string indicating the sampling distribution for the traits. "unif" is a uniform distribution, "norm" is a normal distribution, and "lnorm" is a lognormal distribution.
-# abun.method	- character string indicating the sampling distribution for the species abundances. Same as for tr.method.
-# w.abun - logical; should FDis, FEve, FDiv, and Rao's quadratic entropy (Q) be weighted by species abundances?
+# _Experiment 1:_ Simulate trait values following a standardized normal distribution
 
-res<-simul.dbFD(s = c(5,10,15,20,25,30,35,40,45,50,55,60,65,70,75,80,85,90,95,100), t = 3, 
-           r = 1000, p = 500, tr.method ="norm", w.abun = F)
+set.seed(403) # Fix the seed to be able to reproduce the experiment
 
-res$abun[res$abun > 0] <- 1 
+res1 <- simul.dbFD(s = c(5,10,15,20,25,30,35,40,45,50,55,60,65,70,75,80,85,90,95,100), 
+                   t = 5, r = 1000, p = 500, tr.method ="norm", w.abun = F)
 
-#get sd of traits in assemblages
+results1 <- data.frame(res1$results)
+colnames(results1)[1] <- "Richness"
 
+# Get standard deviation and range of simulates traits in assemblages:
+res1$abun[res1$abun > 0] <- 1 
 SD <- NA
 amp <- NA
-for(i in 1:nrow(res$abun)){
-  tmp <- res$traits[which(res$abun[i,]==1)]
+for(i in 1:nrow(res1$abun)){
+  tmp <- res1$traits[which(res1$abun[i,]==1)]
   SD[i] <- sd(tmp)
   amp[i] <- max(tmp)-min(tmp)
 }
 
-png(paste("traitSim.png",sep=""),width=(1000),height=(1000))
-pairs(cbind(SD, amp, res$results),
+experiment1 <- cbind(SD, amp, results1)
+
+pairs(experiment1,
       lower.panel=panel.smooth, upper.panel=panel.cor,diag.panel=panel.hist)
-dev.off()
+
+
+# _Experiment 2:_ Simulate trait values following a normal distribution
+
+# In the following experiments we simulated trait values using parameters extracted from the distribution of body mass values observed across mammals. We used a modified version of the simul.dbFD function from FD package, which allows the choice of parameters to generate trait values. simul.dbFD.mod can be found at  [github.com/oliveirab/simulation_traits](github.com/oliveirab/simulation_traits)
+
+set.seed(403) # Fix the seed to be able to reproduce the experiment
+
+res2 <- simul.dbFD.mod(s = c(5,10,15,20,25,30,35,40,45,50,55,60,65,70,75,80,85,90,95,100),  
+                       t = 5, r = 1000, p = 500, tr.method ="norm", tr.par = c(5.05, 2.47),  
+                       w.abun = F)
+
+results2 <- data.frame(res2$results)
+colnames(results2)[1] <- "Richness"
+
+# Get standard deviation and range of simulates traits in assemblages:
+res2$abun[res2$abun > 0] <- 1 
+SD <- NA
+amp <- NA
+for(i in 1:nrow(res2$abun)){
+  tmp <- res2$traits[which(res2$abun[i,]==1)]
+  SD[i] <- sd(tmp)
+  amp[i] <- max(tmp)-min(tmp)
+}
+
+experiment2 <- cbind(SD, amp, results2)
+
+pairs(experiment2,
+      lower.panel=panel.smooth, upper.panel=panel.cor,diag.panel=panel.hist)
+
+
+# _Experiment 3:_ Simulate trait values following a log normal distribution
+
+set.seed(403) # Fix the seed to be able to reproduce the experiment
+
+res3 <- simul.dbFD.mod(s = c(5,10,15,20,25,30,35,40,45,50,55,60,65,70,75,80,85,90,95,100),
+                       t = 5, r = 1000, p = 500, tr.method ="lnorm", tr.par = c(5.05, 2.47),  
+                       w.abun = F)
+
+results3 <- data.frame(res3$results)
+colnames(results3)[1] <- "Richness"
+
+# Get standard deviation and range of simulates traits in assemblages:
+res3$abun[res3$abun > 0] <- 1 
+SD <- NA
+amp <- NA
+for(i in 1:nrow(res3$abun)){
+  tmp <- res3$traits[which(res3$abun[i,]==1)]
+  SD[i] <- sd(tmp)
+  amp[i] <- max(tmp)-min(tmp)
+}
+
+experiment3 <- cbind(SD, amp, results3)
+
+pairs(experiment3,
+      lower.panel=panel.smooth, upper.panel=panel.cor,diag.panel=panel.hist)
+
+
+
+# _Experiment 4:_ Simulate trait values following a exponential distribution
+
+set.seed(403) # Fix the seed to be able to reproduce the experiment
+
+res4 <- simul.dbFD.mod(s = c(5,10,15,20,25,30,35,40,45,50,55,60,65,70,75,80,85,90,95,100),  
+                       t = 5, r = 1000, p = 500, tr.method ="exp", tr.par = 2, 
+                       w.abun = F)
+
+results4 <- data.frame(res4$results)
+colnames(results4)[1] <- "Richness"
+
+# Get standard deviation and range of simulates traits in assemblages:
+res4$abun[res4$abun > 0] <- 1 
+SD <- NA
+amp <- NA
+for(i in 1:nrow(res4$abun)){
+  tmp <- res4$traits[which(res4$abun[i,]==1)]
+  SD[i] <- sd(tmp)
+  amp[i] <- max(tmp)-min(tmp)
+}
+
+experiment4 <- cbind(SD, amp, results4)
+
+pairs(experiment4,
+      lower.panel=panel.smooth, upper.panel=panel.cor,diag.panel=panel.hist)
 
 
 save.image("simTrait.RData")
 
-#### END OF CODE ####
+### END OF CODE ###
